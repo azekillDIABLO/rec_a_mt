f2cd4cd11305e93450a544ba2797f864534c684d
Lejo1 <Lejo_1@web.de>
2017-08-10 14:50:06 +0200

    Reopen form when input is invalid.

--- init.lua
+++ init.lua
@@ -3,6 +3,22 @@ local modstorage = core.get_mod_storage()
 local time_zero = 1
 local frame_delay = 0
 
+local rec_form = function()
+	minetest.show_formspec("record", 
+		"size[5,4]" ..
+		"bgcolor[#080808BB;false]" ..
+		"image[-0.35,-0.4;1.2,1.2;camera_btn.png]" ..
+		"button_exit[4.2,-0.15;1,0.7;close;×]" ..
+		"label[0.6,0;Minetest Recording GUI:]" ..
+		"vertlabel[-0.2,0.4;PARAMETERS]" ..
+		"textlist[0.4,1;5,1.5;rec_info;What to do?,Type the number of seconds you want to record!,You will have 2 seconds before records starts!,Recording can generate many data,up to ten-twelve images per second,so be careful and lower the resolution.,--- azekill_DIABLO]" ..
+		"field[0.4,3.1;2.4,1;rec_timer;Timing in seconds:;5]" ..
+		"field[3.08,3.1;2.4,1;rec_fps;Framerate:;10]" ..
+		"button_exit[0.1,3.5;2.43,1;rec_btn;Start]" ..
+		"button[2.8,3.5;2.41,1;stop_btn;Stop]"
+	)
+end
+
 local rec_mt = function(timer, fps)
 	if tonumber(timer) and tonumber(fps) and fps ~= "" and fps > "0" and timer > "0" then
 		local framerate = 1/fps
@@ -22,23 +38,10 @@ local rec_mt = function(timer, fps)
 			end)
 		end)
 	else minetest.display_chat_message("Framerate or the Timing in seconds is invalid!")
+		minetest.after(0.1, function()
+			rec_form()
+		end)
 	end
-end
-
-local rec_form = function()
-	minetest.show_formspec("record", 
-		"size[5,4]" ..
-		"bgcolor[#080808BB;false]" ..
-		"image[-0.35,-0.4;1.2,1.2;camera_btn.png]" ..
-		"button_exit[4.2,-0.15;1,0.7;close;×]" ..
-		"label[0.6,0;Minetest Recording GUI:]" ..
-		"vertlabel[-0.2,0.4;PARAMETERS]" ..
-		"textlist[0.4,1;5,1.5;rec_info;What to do?,Type the number of seconds you want to record!,You will have 2 seconds before records starts!,Recording can generate many data,up to ten-twelve images per second,so be careful and lower the resolution.,--- azekill_DIABLO]" ..
-		"field[0.4,3.1;2.4,1;rec_timer;Timing in seconds:;5]" ..
-		"field[3.08,3.1;2.4,1;rec_fps;Framerate:;10]" ..
-		"button_exit[0.1,3.5;2.43,1;rec_btn;Start]" ..
-		"button[2.8,3.5;2.41,1;stop_btn;Stop]"
-	)
 end
 
 --button pressing
